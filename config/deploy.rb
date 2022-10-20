c6379f9b41dd7f85cd9d3e4cac7bb8af7050a0c2
taikiwatanabe <taiki.watanabe@logiclogic.jp>
2022-10-21 01:54:28 +0900

    デプロイできるようにする

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "3.17.1"
 
 set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:taikiwatanabe0720/my_app.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -37,3 +37,9 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :ssh_options, {
+  auth_methods: ['publickey'], 
+  keys: ['~/.ssh/hundred_poems.pem'] 
+}
+
+set :rbenv_ruby, '2.6.9p207'
