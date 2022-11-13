c8fec7d0ed7e0e5dca5cb37fca10e87aa118581a
taikiwatanabe <taiki.watanabe@logiclogic.jp>
2022-11-14 01:33:18 +0900

    認証関連変えてみる

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/var/www/my_app"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "config/credentials/production.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
